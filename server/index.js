3fb6a215f9306ed161cfdb46952097e478433084
nguyenhau8209 <conghaunguyen909@gmail.com>
2023-10-02 16:23:03 +0700

    add cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const db = require("./models");
 const postRouter = require("./routes/Posts");
 
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Welcome to fullstack SERN");
 });
